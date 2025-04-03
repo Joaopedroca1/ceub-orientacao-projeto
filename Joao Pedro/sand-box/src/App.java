576a936787bddf6f99bffa1bd376d167b2583f6b
João Pedro Carvalho Lima Rocha <joao.rocha21@dinfor.uniceub>
2025-04-03 20:59:24 -0300

    minha pasta

---
+++ Joao Pedro/sand-box/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        ContaBancaria contaNey = new ContaBancaria();
+        contaNey.setSaldo(1000);
+
+
+    }
+}
